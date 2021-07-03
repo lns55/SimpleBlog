631be615b987ab2e073bdce91fe3a413ea0a7b87
lzn747 <lzn747@gmail.com>
2021-07-03 13:26:50 +0300

    Article and comment model created

--- SimpleBlog/app/models.py
+++ SimpleBlog/app/models.py
@@ -4,4 +4,12 @@ Definition of models.
 
 from django.db import models
 
-# Create your models here.
+class Article(models.model):
+    article_title = models.CharField("Article Name", max_lenght = 200)
+    article_text = models.TextField("Article Body")
+    pub_date = models.DateTimeField("Date of publication")
+
+class Comment(models.model):
+    article = models.ForeignKey(Article, on_delete = models.CASCADE)
+    author_name = models.CharField("Author", max_lenght = 50)
+    comment_text = models.CharField("Comment Body", max_lenght = 200)
