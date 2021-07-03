049c9de7ba9d649d1c7ad1440519a0a44524cde5
lzn747 <lzn747@gmail.com>
2021-07-03 13:55:04 +0300

    Minor changes in model. Creating and applying Migration.

--- SimpleBlog/app/models.py
+++ SimpleBlog/app/models.py
@@ -4,12 +4,12 @@ Definition of models.
 
 from django.db import models
 
-class Article(models.model):
-    article_title = models.CharField("Article Name", max_lenght = 200)
+class Article(models.Model):
+    article_title = models.CharField('Article Name', max_length = 200)
     article_text = models.TextField("Article Body")
     pub_date = models.DateTimeField("Date of publication")
 
-class Comment(models.model):
+class Comment(models.Model):
     article = models.ForeignKey(Article, on_delete = models.CASCADE)
-    author_name = models.CharField("Author", max_lenght = 50)
-    comment_text = models.CharField("Comment Body", max_lenght = 200)
+    author_name = models.CharField('Author', max_length = 50)
+    comment_text = models.CharField("Comment Body", max_length = 200)
